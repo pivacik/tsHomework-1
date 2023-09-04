211162719d471b746e214788f6dc3d722278e58d
pivacik <32345383+pivacik@users.noreply.github.com>
2023-09-04 18:08:05 +0400

    fix problem 3

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,6 +1,6 @@
 import { expect, it } from "@jest/globals";
 
-export const getName = (params: { first: string; last: string }) => {
+export const getName = (params: { first: string; last?: string }) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
